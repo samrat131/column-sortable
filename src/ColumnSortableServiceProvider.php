59b65291cca8ed09809d7bffd5a29728faf87e1d
samrat azad <samrat0131@gmail.com>
2020-08-25 03:47:25 +0600

    typo fix

--- src/ColumnSortableServiceProvider.php
+++ src/ColumnSortableServiceProvider.php
@@ -29,7 +29,7 @@ class ColumnSortableServiceProvider extends ServiceProvider
     public function register()
     {
         app()->singleton('sortcolumn', function () {
-            return new SortColumn2;
+            return new SortColumn;
         });
     }
 }
